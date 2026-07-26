8fc5b099fb2cdfe9969c4fcb1531a171863d8d5b
linh24100234 <24100234@st.phenikaa-uni.edu.vn>
2026-07-26 13:08:15 +0700

    Update app.js

--- quanlinhahang/app.js
+++ quanlinhahang/app.js
@@ -44,6 +44,7 @@ const adminReservationRoutes = require('./routes/adminReservation');
 app.use('/', indexRoutes);
 app.use('/auth', authRoutes); // Tự động thêm tiền tố /auth vào các đường dẫn auth
 app.use('/', cartRoutes);
+app.use('/', adminReservationRoutes);
 
 // 5. CHẠY SERVER
 app.listen(PORT, () => {
