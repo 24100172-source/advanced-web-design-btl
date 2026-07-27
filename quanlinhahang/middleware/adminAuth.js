5227097bd3f49bc53a2a5814d13397f5eea9809a
24100172-source <24100172@st.phenikaa-uni.edu.vn>
2026-07-27 09:10:05 +0700

    Update adminAuth.js

--- quanlinhahang/middleware/adminAuth.js
+++ quanlinhahang/middleware/adminAuth.js
@@ -1,8 +1,6 @@
 module.exports = (req, res, next) => {
-  // Kiểm tra xem session có tồn tại và user có phải là admin không
-  if (req.session && req.session.user && req.session.user.role === 'admin') {
-    return next(); // Cho phép đi tiếp vào trang admin
-  }
-  // Nếu chưa đăng nhập hoặc không phải admin thì đá về trang login
+  if (req.session && req.session.admin && req.session.admin.role === 'admin') {
+    return next(); 
+  } 
   res.redirect('/admin/login');
 };
